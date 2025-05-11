0058070d8b851bcfed0e27c680b20ba25b2c1301
Ahmed_mokhtar <ahmedmohamedmokhtarshalape2002@gmail.com>
2025-05-11 16:15:28 +0300

    Initial commit

---
+++ JS/home.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed('.types', {
+      strings: ["Designer", "Developer"],
+      typeSpeed: 90,
+      loop: true
+    });
+  });
